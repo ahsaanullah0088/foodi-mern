c598d35580e684d47cb3d8a67f2acdfa76c23061
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-07-16 21:05:07 +0500

    make special dishes section

--- food-client/src/Pages/home/Home.jsx
+++ food-client/src/Pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import Categories from './Categories'
+import SpecialDishes from './SpecialDishes'
+import Testmonials from './Testimonials'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Categories/>
+      <SpecialDishes/>
+      <Testmonials/>
     </div>
   )
 }
